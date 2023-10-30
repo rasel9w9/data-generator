435990ec2df9cf9f17ea3494a6070df40f0e1b54
Md. Alauddin <rasel9w9@gmail.com>
2023-10-30 09:57:11 +0600

    =jc

--- src/GenerateData.php
+++ src/GenerateData.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace rasel9w9\DataGenerator;
-
+use Illuminate\Support\Facades\Artisan;
 class GenerateData{
     public function generateTableData($table){
+        Artisan::call("alauddin:generate-table-data --table=$table");
     }
 
     public function generateAllData(){
